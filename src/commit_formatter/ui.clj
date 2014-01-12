6129a1ca5554f4b95f672e01b4639e6731ea658c
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-01-12 14:50:50 +0100

    Added handler to the button

--- src/commit_formatter/ui.clj
+++ src/commit_formatter/ui.clj
@@ -1,22 +1,36 @@
 (ns ui
-  (:use [clojure.string :only (split join)]))
+  (:use [clojure.string :only (split join)]
+        [commit-formatter.core :only (format-message)]))
+
+(defmacro on-action [component event & body]
+  `(. ~component addActionListener
+      (proxy [java.awt.event.ActionListener] []
+        (actionPerformed [~event] ~@body))))
 
 (defn create-frame [title size-x size-y]
   (doto(new javax.swing.JFrame)
     (.setSize size-x size-y)
     (.setTitle title)))
 
+(defn update-text-area [text-area]
+  (format "text: %s" (.getText text-area)) 
+  (.setText text-area (format-message (.getText text-area))))
+
 (def header (new javax.swing.JTextField 52))
 
 (defn create-main-frame []
+  (let [message-area (new javax.swing.JTextArea)]
   (doto (create-frame "Commit formatter" 640 480)
     (.add
       (doto (new javax.swing.JPanel (new java.awt.BorderLayout) true)
         (.add header (. java.awt.BorderLayout NORTH))
-        (.add (new javax.swing.JTextArea) 
+        (.add  message-area
               (. java.awt.BorderLayout CENTER))
-        (.add (new javax.swing.JButton "Format & copy") 
-              (. java.awt.BorderLayout SOUTH))))))
+        (.add (doto (new javax.swing.JButton "Format & copy")
+                (on-action event (update-text-area message-area)))
+              (. java.awt.BorderLayout SOUTH)))))))
+
+
 
 (defn new-main-frame []
   (let [frame (create-main-frame)]
