e1051f890f0497f561c39cd5377f30d0f7d24fdd
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-01-14 23:10:36 +0100

    Created separate test directory

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [snipsnap "0.1.0"]
                  [org.clojure/math.combinatorics "0.0.3"]]
-  :plugins [[lein2-eclipse "2.0.0"]])
+  :plugins [[lein2-eclipse "2.0.0"]]
+  :test-selectors {:default (complement :integration)
+                   :integration :integration
+                   :all (fn [_] true)})
