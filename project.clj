d7af37bbb6f56ca6b272590f8a3b8ad0078951ed
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-01-15 23:04:48 +0100

    Version 0.0.1 done.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject commit-formatter "1.0.0-SNAPSHOT"
+(defproject commit-formatter "0.0.1"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [snipsnap "0.1.0"]
