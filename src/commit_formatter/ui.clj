aaed7e75cc2bc7985abc30db90c97d112f532791
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-02-15 22:46:55 +0100

    Added 'Header' and 'Message' labels

--- src/commit_formatter/ui.clj
+++ src/commit_formatter/ui.clj
@@ -2,6 +2,8 @@
   (:use [clojure.string :only (split join)]
         [commit-formatter.core :only (format-message header-length)]
         [snipsnap.core])
+  (:import (javax.swing JPanel)
+           (javax.swing JLabel))
   (:gen-class))
 
 (defmacro on-action [component event & body]
@@ -36,21 +38,32 @@
       (.setText text-area ""))))
 
 (defn create-buttons-panel [format-function clear-function]
-  (doto (new javax.swing.JPanel (new java.awt.FlowLayout) true)
+  (doto (new JPanel (new java.awt.FlowLayout) true)
     (.add (doto (new javax.swing.JButton "Format & copy")
             (on-action event (format-function))))
     (.add (doto (new javax.swing.JButton "Clear")
             (on-action event (clear-function))))))
 
+(defn create-header-panel [header]
+  (doto (new JPanel)
+    (.add (new JLabel "Header:"))
+    (.add header)))
+
+(defn create-message-panel [message-area]
+  (doto (new JPanel)
+    (.add (new JLabel "Message:"))
+    (.add (new javax.swing.JScrollPane message-area))))
+
+
 (defn create-main-frame []
   (let [frame (create-frame "Commit formatter" 640 480)
-        message-area (new javax.swing.JTextArea)
+        message-area (new javax.swing.JTextArea 30 50)
         header (create-header)]
   (doto frame
     (.add
-      (doto (new javax.swing.JPanel (new java.awt.BorderLayout) true)
-        (.add header (. java.awt.BorderLayout NORTH))
-        (.add  (new javax.swing.JScrollPane message-area)
+      (doto (new JPanel (new java.awt.BorderLayout) true)
+        (.add (create-header-panel header) (. java.awt.BorderLayout NORTH))
+        (.add (create-message-panel message-area)
               (. java.awt.BorderLayout CENTER))
         (.add (create-buttons-panel 
                 (format-and-copy-message header message-area)
@@ -59,6 +72,7 @@
 
 (defn new-main-frame []
   (let [frame (create-main-frame)]
+    (.pack frame)
     (.setVisible frame true)))
 
 (defn -main [& args]
