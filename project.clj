b9639de839d01b25a099db59d42e6797764dd5de
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-02-19 21:45:53 +0100

    Updated project configuration

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject commit-formatter "0.1.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [snipsnap "0.1.0"]
-                 [org.clojure/core.match "0.2.1"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [snipsnap "0.1.0" :exclusions[[org.clojure/clojure]]]
+                 [org.clojure/core.match "0.2.1"]
+                 [seesaw "1.4.4"]]
   :plugins [[lein2-eclipse "2.0.0"]]
   :aot [commit-formatter.ui]
   :main commit-formatter.ui
