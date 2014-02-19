092dcc6180de055f655ee5b45e10de8c07499588
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2014-02-19 23:13:22 +0100

    create-buttons-panel rewritten using seesaw

--- src/commit_formatter/ui.clj
+++ src/commit_formatter/ui.clj
@@ -1,7 +1,9 @@
 (ns commit-formatter.ui
   (:use [clojure.string :only (split join)]
+        [clojure.repl]
         [commit-formatter.core]
-        [snipsnap.core])
+        [snipsnap.core]
+        [seesaw.core])
   (:import (javax.swing JPanel)
            (javax.swing JLabel)
            (javax.swing JOptionPane)
@@ -18,7 +20,7 @@
   (doto(new JFrame)
             (.setSize size-x size-y)
             (.setTitle title)
-            (.setDefaultCloseOperation JFrame/DO_NOTHING_ON_CLOSE)))
+            (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)))
 
 (defn create-header []
   (doto (new javax.swing.JTextField header-length)
@@ -30,12 +32,12 @@
                         (proxy-super insertString offset str attr)))))))
   
 (defn format-and-copy-message [header-field text-area]
-  (fn [] (let [formatted-message (format-message (.getText text-area))] 
+  (fn [e] (let [formatted-message (format-message (.getText text-area))] 
            (.setText text-area formatted-message)
            (set-text! (format "%s%s%s" (.getText header-field) header-message-separator formatted-message)))))
 
 (defn clear-message [frame header-field text-area]
-  (fn []
+  (fn [e]
     (when (= 0 (JOptionPane/showConfirmDialog 
                  frame "Are you sure?" "Clear text" JOptionPane/YES_NO_OPTION))
       (.setText header-field "")
@@ -43,7 +45,7 @@
 
 
 (defn paste-message [frame header-field text-area]
-  (fn []
+  (fn [e]
     (let [to-paste (get-text)]
       
       (if (validate-message to-paste)
@@ -56,23 +58,25 @@
           (format "Header must not be empty and cannot be longer than %d characters" header-length) 
           "Format error" JOptionPane/ERROR_MESSAGE)))))
 
+(defn create-button [text function]
+  (doto (button :text text)
+    (listen :action function)))
+
 (defn create-buttons-panel [paste-function format-function clear-function]
-  (doto (new JPanel (new java.awt.FlowLayout) true)
-    (.add (doto (new javax.swing.JButton "Paste")
-            (on-action event (paste-function))))
-    (.add (doto (new javax.swing.JButton "Format & copy")
-            (on-action event (format-function))))
-    (.add (doto (new javax.swing.JButton "Clear")
-            (on-action event (clear-function))))))
+  (flow-panel 
+    :items (list 
+             (create-button "Paste" paste-function)
+             (create-button "Format & copy" format-function)
+             (create-button "Clear" clear-function))))
 
 (defn create-header-panel [header]
   (doto (new JPanel)
-    (.add (new JLabel "Header:"))
+    (.add (label "Header:"))
     (.add header)))
 
 (defn create-message-panel [message-area]
   (doto (new JPanel)
-    (.add (new JLabel "Message:"))
+    (.add (label "Message:"))
     (.add (new javax.swing.JScrollPane message-area))))
 
 
@@ -91,11 +95,12 @@
                   (format-and-copy-message header message-area)
                   (clear-message frame header message-area))
                 (. java.awt.BorderLayout SOUTH))))
-      (.addWindowListener (proxy [WindowAdapter] []
-                            (windowClosing [windowEvent]
-                                           (when (= 0 (JOptionPane/showConfirmDialog 
-                                                        frame "Are you sure?" "Close window" JOptionPane/YES_NO_OPTION))
-                                             (java.lang.System/exit 0))))))))
+;      (.addWindowListener (proxy [WindowAdapter] []
+;                            (windowClosing [windowEvent]
+;                                           (when (= 0 (JOptionPane/showConfirmDialog 
+;                                                        frame "Are you sure?" "Close window" JOptionPane/YES_NO_OPTION))
+;                                             (java.lang.System/exit 0)))))
+      )))
 
 (defn new-main-frame []
   (let [frame (create-main-frame)]
